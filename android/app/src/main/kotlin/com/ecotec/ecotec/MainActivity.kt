368f13f64d9d4d5017540f5d084d0482e7cb6a0a
Jorge Dobkovski <47338154+jorgedobkovski@users.noreply.github.com>
2023-08-22 16:59:34 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/ecotec/ecotec/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ecotec.ecotec
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:ecotec/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  title: "Ecotec",
+  home: Home(),
+  debugShowCheckedModeBanner: false,
+));
